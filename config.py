e101d5fedd38a99ab32f6531877fb4a6f2ac6a73
Axel FoleY <115085855+AxelFoleY11@users.noreply.github.com>
2025-09-07 20:44:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+
 import os
 from dotenv import load_dotenv
+import time
 
 # Загружаем переменные из .env
 load_dotenv()
@@ -14,3 +16,7 @@ MAX_FILE_SIZE = 50 * 1024 * 1024  # 50MB
 # Автоматическое определение пути к FFmpeg
 import shutil
 FFMPEG_PATH = shutil.which("ffmpeg") or "/usr/bin/ffmpeg"
+
+# Кеш подписок
+SUBSCRIPTION_CACHE = {}
+CACHE_TIMEOUT = 300  # 5 минут
